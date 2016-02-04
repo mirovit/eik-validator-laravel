3c7e8bc2186b3d64963d7d83dd7543ce2dea4a2c
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-04 15:13:25 -0500

    Applied fixes from StyleCI

--- src/EIKValidator/EIKValidatorServiceProvider.php
+++ src/EIKValidator/EIKValidatorServiceProvider.php
@@ -3,25 +3,23 @@
 namespace Mirovit\EIKValidator;
 
 use Illuminate\Support\ServiceProvider;
-use Mirovit\EIKValidator\EIKValidator;
 
-class EIKValidatorServiceProvider extends ServiceProvider {
+class EIKValidatorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $validator = function ($attribute, $value, $parameters) {
+            try {
+                return (new EIKValidator())->isValid($$value);
+            } catch (\Exception $e) {
+                return false;
+            }
+        };
 
-	public function boot()
-	{
-		$validator = function($attribute, $value, $parameters)
-		{
-			try {
-				return (new EIKValidator)->isValid($$value);
-			} catch(\Exception $e) {
-				return false;
-			}
-		};
+        $this->app['validator']->extend('eik', $validator);
+        $this->app['validator']->extend('bulstat', $validator);
+    }
 
-		$this->app['validator']->extend('eik', $validator);
-		$this->app['validator']->extend('bulstat', $validator);
-	}
-	
     public function register()
     {
     }
